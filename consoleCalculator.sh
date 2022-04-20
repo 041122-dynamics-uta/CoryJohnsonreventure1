39f8e7c01c75a9a602488523e1148e17907caead
CoryJohnsonReventure <coey540@reventure.net>
2022-04-19 20:53:28 -0400

    consoleCalculator

---
+++ consoleCalculator.sh
@@ -0,0 +1,55 @@
+# !/bin/bash
+ 
+
+
+
+ Calculator ( ) {
+echo "Enter Two numbers : "
+read a
+read b
+ 
+echo "Enter Choice :"
+echo "1. Addition"
+echo "2. Subtraction"
+echo "3. Multiplication"
+echo "4. Division"
+read ch
+ 
+
+case $ch in
+  1)
+    res=$((a + b)) 
+    ;;
+  2)
+    res=$((a - b)) 
+    ;;
+  3)
+    res=$((a \* b)) 
+    ;;
+  4)
+    res=$((a / b)) 
+    ;;
+esac
+echo $res
+echo "Would you like to try another calculation?(y/n)"
+Select
+
+}
+
+Select(){
+read input
+if [[ "$input" == Y || "$input" == y ]]
+    then
+      Calculator
+elif [[ "$input" == N || "$input" == n ]]
+    then
+        exit
+
+fi
+}
+
+Calculator
+
+
+
+
