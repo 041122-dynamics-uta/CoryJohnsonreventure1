d56d03f9e34425e4beb700477e0c79530c6f89a1
CoryJohnsonReventure <coey540@reventure.net>
2022-04-20 19:20:43 -0400

    switchDemo

---
+++ switchMenu/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+//using System.Console;
+
+namespace switchMenu
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Make your Selection,please");
+            int Selection = 1;
+
+            switch (Selection) 
+            {
+            case 1:
+	    
+	          Console.WriteLine("Dial 1");
+            break;
+            case 2:
+   	 
+          	Console.WriteLine("Dial 2");
+            break;
+            case 3:
+    	
+	            Console.WriteLine("Dial 3");
+              break;
+            }
+
+        }
+    }
+}
